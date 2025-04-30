5c812178e055593794a747121bf4d1607eb00fdd
Mauricio Chaves <mauricio.chaves.jr@gmail.com>
2025-04-29 21:15:07 -0300

    main: first commit

---
+++ src/main/java/com/tds/urls_tds_company/UrlsTdsCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.tds.urls_tds_company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlsTdsCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlsTdsCompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tds/urls_tds_company/UrlsTdsCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tds.urls_tds_company;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlsTdsCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
