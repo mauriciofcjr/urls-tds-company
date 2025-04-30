aea63c6ff392fa1abf6135ba83002d3619fa6462
Mauricio Chaves <mauricio.chaves.jr@gmail.com>
2025-04-29 21:25:06 -0300

    develop: Criação do model Urls

---
+++ src/main/java/com/tds/urls_tds_company/model/Urls.java
@@ -0,0 +1,25 @@
+package com.tds.urls_tds_company.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Urls {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String url;
+
+    private String shortUrl;
+
+}
