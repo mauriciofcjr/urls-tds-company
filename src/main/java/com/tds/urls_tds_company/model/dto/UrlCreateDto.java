44622a2dfe8dac009bbde2eb24a06b4d9656eefd
Mauricio Chaves <mauricio.chaves.jr@gmail.com>
2025-05-01 11:16:48 -0300

    develop: Configuração do been Validation

--- src/main/java/com/tds/urls_tds_company/model/dto/UrlCreateDto.java
+++ src/main/java/com/tds/urls_tds_company/model/dto/UrlCreateDto.java
@@ -1,14 +1,21 @@
 package com.tds.urls_tds_company.model.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class UrlCreateDto {
     
+    @NotBlank
     private String url;
+    @NotBlank
     private String shortUrl;
 }
--- src/main/java/com/tds/urls_tds_company/web/controller/UrlController.java
+++ src/main/java/com/tds/urls_tds_company/web/controller/UrlController.java
@@ -13,6 +13,7 @@ import com.tds.urls_tds_company.model.dto.UrlResponseDto;
 import com.tds.urls_tds_company.model.dto.mapper.UrlMapper;
 import com.tds.urls_tds_company.service.UrlService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -23,7 +24,7 @@ public class UrlController {
     private final UrlService urlService;
 
     @PostMapping
-    public ResponseEntity<UrlResponseDto> createUrl(@RequestBody UrlCreateDto urlCreateDto) {
+    public ResponseEntity<UrlResponseDto> createUrl(@Valid @RequestBody UrlCreateDto urlCreateDto) {
         Url url = urlService.createUrl(UrlMapper.toUrl(urlCreateDto));
         UrlResponseDto urlResponseDto = UrlMapper.toDto(url);
         return ResponseEntity.status(HttpStatus.CREATED).body(urlResponseDto);
